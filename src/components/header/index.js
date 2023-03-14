ff05c5bf4599fee6d9279c7b8899c7903827acbf
RaiSufian <mashancs571@gmail.com>
2023-03-14 12:38:56 +0500

    set header responsive

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,25 +1,40 @@
-import React from "react";
+
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import Hamburger from "hamburger-react";
 
 const Header = () => {
+    const [isOpen, setOpen] = useState(false)
+    useEffect(() => {
+        let screen = window.innerWidth;
+        if ("1023" < screen) {
+            console.log("I am working")
+            setOpen(true);
+            
+        }
+    }, [])
     return (
         <div className="bg-gray-900 text-white">
-            <div className="lg:w-[1260px] max-w-full mx-auto">
+            <div className="lg:w-[1260px] max-w-full mx-auto relative ">
                 <div className="flex items-center justify-between">
-                    <div className="main_logo">
+                    <div className="main_logo relative z-50">
                         <img src="./images/logo.png" width={117} height={35} />
                     </div>
                     <div className="main_menu">
-                        <ul class="flex flex-row items-center py-6 py-3">
-                            <li class="mx-2"><Link to="/" className="py-3 px-4 text-sm font-bold font-mont">Home</Link></li>
-                            <li class="mx-2"><Link to="/aboutus" className="py-3 px-4 text-sm font-bold">About Us</Link></li>
-                            <li class="mx-2"><Link to="/" className="py-3 px-4 text-sm font-bold">About</Link></li>
-                            <li class="mx-2"><Link to="/" className="py-3 px-4 text-sm font-bold">Services</Link></li>
-                            <li class="mx-2"><Link to="/" className="py-3 px-4 text-sm font-bold">Contact</Link></li>
+                        <ul className={`flex gap-4 lg:gap-0 lg:flex-row flex-col absolute  lg:static left-0 top-full w-full bg-gray-900 items-center py-6 py-3 animate-menu_ani ${isOpen ? null : 'hidden'}`}>
+                            <li className="mx-2"><Link to="/" className="py-3 px-4 text-sm font-bold font-mont">Home</Link></li>
+                            <li className="mx-2"><Link to="/aboutus" className="py-3 px-4 text-sm font-bold">About Us</Link></li>
+                            <li className="mx-2"><Link to="/" className="py-3 px-4 text-sm font-bold">Events</Link></li>
+                            <li className="mx-2"><Link to="/" className="py-3 px-4 text-sm font-bold">Content</Link></li>
+                            <li className="mx-2"><Link to="/" className="py-3 px-4 text-sm font-bold">Support us</Link></li>
                         </ul>
                     </div>
                     <div className="">
-                        <button className="bg-[#7230b0] py-3 px-5 rounded-full font-bold font-lato leading-none hover:bg-black hover:text-white">Support us</button>
+                        <div className="lg:hidden">
+                            <Hamburger toggled={isOpen} toggle={setOpen} />
+                        </div>
+
+                        <button className="bg-[#7230b0] py-3 px-5 rounded-full font-bold font-lato leading-none hover:bg-black hover:text-white hidden lg:block">Support us</button>
                     </div>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
     extend: {
       boxShadow: {
         'shadow1': 'rgb(0, 0, 0) 0px 6px 13px -10px',
+      },
+      animation: {
+        'menu_ani': 'menu 0.3s linear',
+      },
+      keyframes: {
+        menu: {
+          '0%': { transform: 'translateY(-100%)' },
+          '100%': { transform: 'translateY(0%)' },
+        }
       }
     },
     fontFamily: {
