1b2c411c39dceae1861544af01606088d7e9c077
RaiSufian <mashancs571@gmail.com>
2023-04-13 00:47:35 +0500

    add helmet in project

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -3,14 +3,21 @@ import Importance from "./improtance";
 import Message from "./mainMsg";
 import InvestGuide from "./InvestGuide";
 import ProjectSlider from "./projectsSlider";
+import { Helmet } from 'react-helmet';
 const Home = () => {
     return (
-        <>  <HeroSection />
+        <>
+            < Helmet >
+                <title>V Crowd Fund | The Future of Property Investment</title>
+                <meta name="description"
+                    content="Discover the future of property investment with London House Exchange, The world’s only regulated stock exchange for individual properties. Property ownership, simplified."/>
+            </Helmet>
+            <HeroSection />
             <Importance />
             <Message />
             <InvestGuide />
-            <ProjectSlider aboutPage={"Active Investment Opportunities"} status={1}/>
-            <ProjectSlider aboutPage={"Past Investment Opportunities"} status={0}/>
+            <ProjectSlider aboutPage={"Active Investment Opportunities"} status={1} />
+            <ProjectSlider aboutPage={"Past Investment Opportunities"} status={0} />
         </>
     )
 }
